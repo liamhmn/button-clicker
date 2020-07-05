0c53584f29ba9962ef714cda64200d6522e8273e
Mark <mark@remarkablemark.org>
2020-07-04 20:18:47 -0400

    gitploy

---
+++ precache-manifest.8133059538d269395c96aa1679e40bf7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb35dba9e59df000901705e86f813be1",
+    "url": "/button-clicker/index.html"
+  },
+  {
+    "revision": "b7da9cace7d0d99e6599",
+    "url": "/button-clicker/static/css/main.61459a1e.chunk.css"
+  },
+  {
+    "revision": "b7da9cace7d0d99e6599",
+    "url": "/button-clicker/static/js/main.99fc1384.chunk.js"
+  },
+  {
+    "revision": "d695a2c7be424c974fb9",
+    "url": "/button-clicker/static/js/runtime-main.74a5e7b2.js"
+  }
+]);
